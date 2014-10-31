a313173a87ea06d2f430a3605e564301f3d794fc
stiderspace <stiderspace@gmail.com>
2014-10-31 17:32:56 +0100

    BugFix #1
    mostly spell corections

--- Common/spaceCoder/riftcraft/lib/reference/Reference.java
+++ Common/spaceCoder/riftcraft/lib/reference/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
     public static final String MOD_ID = "Riftcraft";
     public static final String MOD_NAME = "Riftcraft";
-    public static final String FINGERPRINt = "@Fingerprint@";
+    public static final String FINGERPRINT = "@Fingerprint@";
     public static final String VERSION_NUMBER = "1.0";
     public static final String SERVER_PROXY_CLASS = "spaceCoder.riftcraft.proxy.ServerProxy";
     public static final String CLIENT_PROXY_CLASS = "spaceCoder.riftcraft.proxy.ClientProxy";
