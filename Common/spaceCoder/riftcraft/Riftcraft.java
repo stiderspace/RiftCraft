6df22e60f5d0c65f887c9b91c5427bc915b8f91e
stiderspace <stiderspace@gmail.com>
2015-10-20 09:03:21 +0200

    small tweek

--- Common/spaceCoder/riftcraft/Riftcraft.java
+++ Common/spaceCoder/riftcraft/Riftcraft.java
@@ -1,16 +1,5 @@
 package spaceCoder.riftcraft;
 
-import spaceCoder.riftcraft.crafting.CraftingItems;
-import spaceCoder.riftcraft.crafting.ModCrafting;
-import spaceCoder.riftcraft.handler.ConfigurationHandler;
-import spaceCoder.riftcraft.init.ModBlocks;
-import spaceCoder.riftcraft.init.ModDimensions;
-import spaceCoder.riftcraft.init.ModItems;
-import spaceCoder.riftcraft.init.ModTile;
-import spaceCoder.riftcraft.init.ModWorldGeneration;
-import spaceCoder.riftcraft.lib.reference.Reference;
-import spaceCoder.riftcraft.lib.utility.LogHelper;
-import spaceCoder.riftcraft.proxy.CommonProxy;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
@@ -19,6 +8,17 @@ import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import spaceCoder.riftcraft.crafting.CraftingItems;
+import spaceCoder.riftcraft.crafting.ModCrafting;
+import spaceCoder.riftcraft.handler.ConfigurationHandler;
+import spaceCoder.riftcraft.init.ModBlocks;
+import spaceCoder.riftcraft.init.ModItems;
+import spaceCoder.riftcraft.init.ModOreRegistation;
+import spaceCoder.riftcraft.init.ModTile;
+import spaceCoder.riftcraft.init.ModWorldGeneration;
+import spaceCoder.riftcraft.lib.reference.Reference;
+import spaceCoder.riftcraft.lib.utility.LogHelper;
+import spaceCoder.riftcraft.proxy.CommonProxy;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION_NUMBER, guiFactory = Reference.GUI_FACTORY_CLASS)
 
@@ -43,6 +43,7 @@ public class Riftcraft {
             //ModDimensions.init();
             ModCrafting.init();
             CraftingItems.init();
+            ModOreRegistation.init();
             LogHelper.info("Pre Initialization Finished");
 		    		
 		}
